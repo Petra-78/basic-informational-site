778858fc9349fac53c99b88f0229d173dae2bed6
Petra-78 <partos.petra.0305@gmail.com>
2025-11-17 16:51:07 +0100

    Change routing logic to express

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-const http = require("node:http");
-const fs = require("node:fs");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  console.log("Requested URL:", req.url);
-  res.writeHead(200, { "Content-type": "text/html" });
-
-  let path = "./";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact":
-      path += "contact-me.html";
-      break;
-    default:
-      path += "404.html";
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.end();
-    }
-    res.end(data);
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-server.listen(8080);
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/contact-me.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/about.html"));
+});
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "public/404.html"));
+});
+
+app.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
+});
